224826e1b8c84d52d35a914b93d1fb7cfbfd0455
Arpit Shrotriya <arpit.shrotriya5945@gmail.com>
2021-07-12 05:44:34 +0530

    model_working

---
+++ lib/home.dart
@@ -0,0 +1,132 @@
+import 'package:camera/camera.dart';
+import 'package:face_mask_detector/main.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late CameraImage imgCamera;
+  late CameraController cameraController;
+  bool isWorking = false;
+  String result = "";
+
+  initCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+
+    cameraController.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+
+      setState(() {
+        cameraController.startImageStream((imageFromStream) => {
+              if (!isWorking)
+                {
+                  isWorking = true,
+                  imgCamera = imageFromStream,
+                  runModelOnFrame(),
+                }
+            });
+      });
+    });
+  }
+
+  loadmodel() async {
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  runModelOnFrame() async {
+    // ignore: unnecessary_null_comparison
+    if (imgCamera != null) {
+      var recognitions = await Tflite.runModelOnFrame(
+        bytesList: imgCamera.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: imgCamera.height,
+        imageWidth: imgCamera.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 1,
+        threshold: 0.1,
+        asynch: true,
+      );
+
+      result = "";
+
+      recognitions!.forEach((response) {
+        result += response["label"] + "\n";
+      });
+
+      setState(() {
+        result;
+      });
+
+      isWorking = false;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    initCamera();
+    loadmodel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Padding(
+              padding: const EdgeInsets.only(top: 40.0),
+              child: Center(
+                child: Text(
+                  result,
+                  style: const TextStyle(
+                    backgroundColor: Colors.black54,
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          body: Column(
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                width: size.width,
+                height: size.height - 100,
+                child: Container(
+                  height: size.height - 100,
+                  child: (!cameraController.value.isInitialized)
+                      ? Container()
+                      : AspectRatio(
+                          aspectRatio: cameraController.value.aspectRatio,
+                          child: CameraPreview(cameraController),
+                        ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:face_mask_detector/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription>? cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Face Mask Detection',
+      home: MySplashPage(),
+    );
+  }
+}
